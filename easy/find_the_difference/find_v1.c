1fe2359eeaea3bc38d16d784cff60f544afead1f
Seiji <rf.seiji.ueno@gmail.com>
2023-09-25 19:39:30 -0300

    daily problem - find the difference, 0 ms - XoR method

---
+++ easy/find_the_difference/find_v1.c
@@ -0,0 +1,23 @@
+/*
+    389. Find the Difference - LeetCode
+    URL: https://leetcode.com/problems/find-the-difference/description/?envType=daily-question&envId=2023-09-25
+    
+    Language: C
+    Method: XoR
+    Time complexity of O(n)
+
+    Author: Seiji - 24/09/23
+*/
+
+char findTheDifference(char * s, char * t){
+    char result = 0;
+    while (*s != '\0') {
+        result ^= *s;
+        s++;
+    }
+    while (*t != '\0') {
+        result ^= *t;
+        t++;
+    }
+    return result;
+}
